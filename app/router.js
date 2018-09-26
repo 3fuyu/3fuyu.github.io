d0f058ff18a2329d20ae890ebed16e4dce40bd8b
yehua <yehua.zy@alibaba-inc.com>
2018-09-26 17:10:31 +0800

    feat: update

---
+++ app/router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const { router, controller } = app;
+  router.get('/', controller.home.index);
+  router.get('/publish', controller.home.publish);
+  router.post('/publish', controller.home.publish);
+};
