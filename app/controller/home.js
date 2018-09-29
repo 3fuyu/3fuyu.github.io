a3b906eb8d73151173544f1c9c70268c9b40c8ae
yehua <yehua.zy@alibaba-inc.com>
2018-09-29 16:05:06 +0800

    feat: add publish token

--- app/controller/home.js
+++ app/controller/home.js
@@ -20,18 +20,28 @@ class HomeController extends Controller {
     await ctx.render('index.html', data);
   }
 
+  async issueHelper() {
+    await this.ctx.render('issue_helper.html');
+  }
+
   async publish() {
     const { ctx } = this;
+    const token = '__dt_wl__';
     const diamond = JSON.parse(fs.readFileSync(path.join(__dirname, '../assets/diamond.json'), 'utf-8'));
     const data = ctx.request.method === 'GET' ? ctx.query : ctx.request.body;
-    const result = {
-      ...diamond,
-      ...data || {}
-    };
+    if (data.token === token) {
+      delete data.token;
+      const result = {
+        ...diamond,
+        ...data
+      };
 
-    fs.writeFileSync(path.join(__dirname, '../assets/diamond.json'), JSON.stringify(result));
+      fs.writeFileSync(path.join(__dirname, '../assets/diamond.json'), JSON.stringify(result));
 
-    ctx.body = result;
+      ctx.body = result;
+    } else {
+      ctx.body = 'Github: https://github.com/NG-ZORRO/ng-zorro-antd-mobile';
+    }
   }
 }
 
